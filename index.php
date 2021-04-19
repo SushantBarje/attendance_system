1a750fc660a432aa478158af420ea9330993c37c
MUNGASE ROHIT YUVRAJ <79807799+RYM1999@users.noreply.github.com>
2021-04-19 13:47:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>skn online attendence</title>
+	<link rel="stylesheet" href="style.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+     
+</head>
+
+<body>
+
+        
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link">SKN Sinhgad College Of Engineering Korti,Pandharpur</a>
+    </li>
+  
+  </ul>
+</nav>
+	<center>
+
+<header>
+
+  <h1>Online Attendance System </h1>
+
+</header>
+
+</br></br>
+
+<div class="back">
+
+
+  <div class="div-center">
+      
+
+    <div class="content">
+
+
+      <h3>Student Login</h3>
+      <hr />
+
+      <form class="justify-content-center">
+        <div class="form-group">
+		
+          <label for="exampleInputEmail1">Student Username</label>
+          <input type="username" class="form-control" id="exampleInputusername" placeholder="Enter Your PRN " >
+        </div>
+        
+        
+        <button type="submit" class="btn btn-primary">Login</button>
+   
+        
+
+      </form>
+
+    </div>
+
+
+    </span>
+  </div>
+</body>
---
+++ student.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>skn online attendence</title>
+	<link rel="stylesheet" href="style.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+        a:visited {
+          color: green;
+        }
+        
+        
+        a:link {
+              text-decoration: none;
+        }
+        a:link, a:visited {
+  background-color: white;
+  color: black;
+  border: 2px solid green;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+a:hover, a:active {
+  background-color: green;
+  color: white;
+}
+}
+        </style>
+
+
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link">SKN Sinhgad College Of Engineering Korti,Pandharpur</a>
+      <button type="submit" class="btn btn-primary">Logout</button>
+    </li>
+  
+  </ul>
+</nav>
+
+<center>
+
+<header>
+</br>
+  <h1>Student Panel </h1>
+</br></br>
+  <div class="report">
+  <a href="">Generate Report </a>
+</div>
+</br></br>
+<div class="view">
+  <a href="">View attendence </a>
+</div>
+</br></br>
+
+</header>
+
+</body>
+    </html>
