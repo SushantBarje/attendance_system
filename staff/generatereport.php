cdcef90292c2725aef63cbc2d27d8815b680a3e8
Sushant Barje <sushantbarje4@gmail.com>
2021-05-26 17:09:05 +0530

    staff_report_module

--- staff/generatereport.php
+++ staff/generatereport.php
@@ -7,24 +7,7 @@
 <body>
 <?php   include "staffHeader.php" ?>
 <main>
-    <label for="course"><b>Course</b></label>
-        <select name="dept" id="dept" form="dept">
-            <option value="na">NA</option>
-            <option value="ai">AI</option>
-            <option value="unix">UNIX </option>
-            <option value="oomd">OOMD</option>
-            <option value="cc">CC</option>
-            <option value="coa">COA</option>
-        </select>
-        <label for="course"><b>Attendence</b></label>
-        <select name="dept" id="dept" form="dept">
-            <option value="na">NA</option>
-            <option value="cse">Theory</option>
-            <option value="cse">Practical</option>
-        </select>
-        <label for="datetime"><b>Date and Time:</b></label>
-        <input type="datetime-local" id="birthdaytime" name="birthdaytime">
-        <input type="submit">
+    
 </main>
 </body>
 </html>
